2a6b7249b155b6aa7995ff8f51f8ebca7501bb25
Ben Glazer <benglazer@indeed.com>
2017-11-06 06:39:51 +0000

    bootstrap: backup dotfiles and unblock stow

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,10 @@ sudo apt-get upgrade
 sudo apt-get install -y git stow
 sudo apt-get install -y "linux-headers-$(uname -r)" build-essential dkms
 
+# Backup default dotfiles (and get them out of stow's way)
+mkdir "$HOME/dotfiles-orig/"
+mv -iv .bash_logout .bashrc .profile "$HOME/dotfiles-orig/"
+
 # Pull down and install dotfiles
 git clone https://github.com/benglazer/dotfiles-ubuntu.git "$HOME/.dotfiles"
 cd "$HOME/.dotfiles"
