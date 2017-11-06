e0754dfe1f0d9ac9c118437cf87b4a7fb992da39
Ben Glazer <benglazer@indeed.com>
2017-11-06 13:03:03 +0000

    installers: make virtualbox-guest-additions.sh directly sourceable

--- installers/virtualbox-guest-additions.sh
+++ installers/virtualbox-guest-additions.sh
@@ -1,15 +1,15 @@
-install_virtualbox_guest_additions() {
-    version=${1:-$(VBoxClient --version | sed 's/r[0-9]\+//g')}
-    if [ -z "$version" ]; then
-        echo "Usage: install_virtualbox_guest_additions <VIRTUALBOX_VERSION>"
-        return 1
-    fi
-    echo "Installing Guest Additions for Virtualbox version '$version'"
-    wget http://download.virtualbox.org/virtualbox/$version/VBoxGuestAdditions_$version.iso
-    sudo mkdir /media/VBoxGuestAdditions
-    sudo mount -o loop,ro VBoxGuestAdditions_$version.iso /media/VBoxGuestAdditions
-    sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-    sudo umount /media/VBoxGuestAdditions
-    sudo rmdir /media/VBoxGuestAdditions
-    rm VBoxGuestAdditions_$version.iso
-}
+# Use command-line param as version number, otherwise introspect
+version=${1:-$(VBoxClient --version | sed 's/r[0-9]\+//g')}
+if [ -z "$version" ]; then
+    echo "Usage: install_virtualbox_guest_additions <VIRTUALBOX_VERSION>"
+    return 1
+fi
+
+echo "Installing Guest Additions for Virtualbox version '$version'"
+wget http://download.virtualbox.org/virtualbox/$version/VBoxGuestAdditions_$version.iso
+sudo mkdir /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_$version.iso /media/VBoxGuestAdditions
+sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
+sudo umount /media/VBoxGuestAdditions
+sudo rmdir /media/VBoxGuestAdditions
+rm VBoxGuestAdditions_$version.iso
