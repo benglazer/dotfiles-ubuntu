c520a41132d2af220a1e684b80f45105b2c954d5
Ben Glazer <benglazer@indeed.com>
2017-11-06 11:42:52 +0000

    bootstrap: improve output

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,21 +9,22 @@ pushd . > /dev/null  # store original working dir
 # Useful system variables
 release_name=$(lsb_release -c | awk '{print $2}')
 
-
-# Update apt
+echo Updating apt
 sudo apt-get update
 sudo apt-get upgrade
 
-# Ensure all dependencies are installed
-sudo apt-get install -y git stow
+echo Ensuring all build dependencies are installed
 sudo apt-get install -y "linux-headers-$(uname -r)" build-essential dkms
+sudo apt-get install -y git stow
 
-# Backup default dotfiles (and get them out of stow's way)
+echo "Backing up default dotfiles (and getting them out of stow's way)"
 mkdir "$HOME/dotfiles-orig/"
 mv -iv .bash_logout .bashrc .profile "$HOME/dotfiles-orig/"
 
-# Pull down and install dotfiles
+echo Pulling down dotfiles repo
 git clone https://github.com/benglazer/dotfiles-ubuntu.git "$HOME/.dotfiles"
+
+echo Installing dotfiles via stow
 cd "$HOME/.dotfiles"
 stow bash git vim
 
