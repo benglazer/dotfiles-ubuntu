64d702d66b8848c7f044f56dbfc2911fb45445dd
Ben Glazer <benglazer@indeed.com>
2017-11-06 12:39:03 +0000

    bootstrap: refactor and improve script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,33 @@
 # Install ubuntu-dotfiles on a new server.
 # Assumes nothing more than apt-get is properly installed.
 
-pushd . > /dev/null  # store original working dir
+# Configuration
+DOTFILES_ROOT="$HOME/.dotfiles"
+DOTFILES_ORIG_DIR="$HOME/dotfiles-orig"
+DOTFILES_REPO="https://github.com/benglazer/dotfiles-ubuntu.git"
 
+# Store original working dir
+pushd . > /dev/null
 
 # Useful system variables
 release_name=$(lsb_release -c | awk '{print $2}')
+
+
+install_dotfiles() {
+    sudo apt-get install -y git stow
+
+    echo -e "\tBacking up default dotfiles (and getting them out of stow's way)"
+    mkdir -pv "$DOTFILES_ORIG_DIR"
+    mv -iv .bash_logout .bashrc .profile "$DOTFILES_ORIG_DIR"
+
+    echo -e "\tPulling down dotfiles repo"
+    git clone "$DOTFILES_REPO" "$DOTFILES_ROOT"
+
+    echo -e "\tInstalling dotfiles via stow"
+    mkdir -pv "$DOTFILES_ROOT"
+    cd "$DOTFILES_ROOT"
+    stow bash git vim
+}
 
 echo Updating apt
 sudo apt-get update
@@ -15,18 +37,16 @@ sudo apt-get upgrade
 
 echo Ensuring all build dependencies are installed
 sudo apt-get install -y "linux-headers-$(uname -r)" build-essential dkms
-sudo apt-get install -y git stow
 
-echo "Backing up default dotfiles (and getting them out of stow's way)"
-mkdir "$HOME/dotfiles-orig/"
-mv -iv .bash_logout .bashrc .profile "$HOME/dotfiles-orig/"
+echo Installing new software
+source "$DOTFILES_ROOT/installers/pyenv.sh"
+source "$DOTFILES_ROOT/installers/virtualbox-guest-additions.sh"
 
-echo Pulling down dotfiles repo
-git clone https://github.com/benglazer/dotfiles-ubuntu.git "$HOME/.dotfiles"
+echo Installing dotfiles
+install_dotfiles
 
-echo Installing dotfiles via stow
-cd "$HOME/.dotfiles"
-stow bash git vim
+echo Restoring original working dir
+popd > /dev/null
 
-
-popd > /dev/null  # restore original working dir
+echo Bootstrap complete. You may reboot the VM to verify everything is
+echo working as expected.
