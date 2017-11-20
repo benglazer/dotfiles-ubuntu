b4a5dfe63e69e26e19c8dabe1a40295617a0773b
Ben Glazer <benglazer@indeed.com>
2017-11-19 18:57:04 -0600

    added pyenv functions and install python2 and python3

--- bash/.profile-extensions/pyenv.sh
+++ bash/.profile-extensions/pyenv.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+# Custom functions
+
+latest_python_stable() {
+    pyenv install -l | sed -nr 's/^  ([\.0-9]+)$/\1/p' | tail -n 1
+}
+
+latest_python2_stable() {
+    pyenv install -l | sed -nr 's/^  (2\.[\.0-9]+)$/\1/p' | tail -n 1
+}
+
+latest_python3_stable() {
+    pyenv install -l | sed -nr 's/^  (3\.[\.0-9]+)$/\1/p' | tail -n 1
+}
+
+mkvenvhere() {
+    envname=${1:-$(basename $(pwd))}
+    (pyenv virtualenv $(latest_python3_stable) $envname) && (echo $envname > .python-version)
+}
--- installers/pyenv.sh
+++ installers/pyenv.sh
@@ -3,3 +3,6 @@
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev  # via https://github.com/pyenv/pyenv/wiki/Common-build-problems
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash  # via https://github.com/pyenv/pyenv-installer
 
+source ../bash/.profile-extensions/pyenv.sh
+pyenv install $(latest_python2_stable)
+pyenv install $(latest_python3_stable)
