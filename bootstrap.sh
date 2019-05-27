838252c0879ef59c5ea1626819f234b7a590dcd0
Ben Glazer <ben@benglazer.com>
2019-05-27 11:23:47 -0500

    boostrap: consistent variable interpolation syntax

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ set -x  # debug
 # Assumes nothing more than apt-get is properly installed.
 
 # Configuration
-DOTFILES_ROOT="$HOME/.dotfiles"
+DOTFILES_ROOT="${HOME}/.dotfiles"
 DOTFILES_REPO="https://github.com/benglazer/dotfiles-ubuntu.git"
 DOTFILES_BACKUP="${HOME}/.dotfiles-backup"
 STOW_GROUPS_TO_INSTALL="bash git vim"
@@ -34,13 +34,13 @@ backup_dotfiles() {
 install_dotfiles() {
     echo "Installing dotfiles"
     sudo apt-get install -y git stow vim
-    git clone "$DOTFILES_REPO" "$DOTFILES_ROOT"
+    git clone "${DOTFILES_REPO}" "${DOTFILES_ROOT}"
 
     backup_dotfiles
 
     echo "Installing dotfiles via stow..."
-    mkdir -pv "$DOTFILES_ROOT"
-    cd "$DOTFILES_ROOT"
+    mkdir -pv "${DOTFILES_ROOT}"
+    cd "${DOTFILES_ROOT}"
     stow ${STOW_GROUPS_TO_INSTALL}
 
     echo "Dotfiles successfully installed"
@@ -50,7 +50,7 @@ pushd . > /dev/null
 
 sudo apt-get update && apt-get install -y "linux-headers-$(uname -r)" build-essential dkms
 install_dotfiles
-source "$DOTFILES_ROOT/installers/pyenv.sh"
+source "${DOTFILES_ROOT}/installers/pyenv.sh"
 
 popd > /dev/null
 echo Bootstrap complete. Reboot to verify everything is working as expected.
