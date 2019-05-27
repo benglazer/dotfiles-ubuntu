fb8d9980d2fe732937edef5b4fe7e76f6ec9028f
Ben Glazer <ben@benglazer.com>
2019-05-27 11:20:12 -0500

    bootstrap: more robust dotfiles backup (but overwrites existing backups)

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,38 @@ set -x  # debug
 # Configuration
 DOTFILES_ROOT="$HOME/.dotfiles"
 DOTFILES_REPO="https://github.com/benglazer/dotfiles-ubuntu.git"
+DOTFILES_BACKUP="${HOME}/.dotfiles-backup"
+STOW_GROUPS_TO_INSTALL="bash git vim"
+
+backup_dotfiles() {
+    echo "Backing up dotfiles"
+    pushd . > /dev/null
+    cd "${DOTFILES_ROOT}"
+    for dotfile_group in ${STOW_GROUPS_TO_INSTALL}; do
+        cd "${dotfile_group}"
+        for relative_dotfile_path in $(find . -type f) ; do
+            if [[ -x "${HOME}/${relative_dotfile_path}" ]] ; then
+                echo mkdir -pv "$("${DOTFILES_BACKUP}/${dotfile_group}/${relative_dotfile_path}")"
+                echo mv -v "${HOME}/${relative_dotfile_path}" \
+                           "${DOTFILES_BACKUP}/${dotfile_group}/${relative_dotfile_path}"
+            fi
+        done
+        cd ..
+    done
+    popd > /dev/null
+}
 
 install_dotfiles() {
     echo "Installing dotfiles"
     sudo apt-get install -y git stow vim
     git clone "$DOTFILES_REPO" "$DOTFILES_ROOT"
 
+    backup_dotfiles
+
     echo "Installing dotfiles via stow..."
     mkdir -pv "$DOTFILES_ROOT"
     cd "$DOTFILES_ROOT"
-    stow bash git vim
+    stow ${STOW_GROUPS_TO_INSTALL}
 
     echo "Dotfiles successfully installed"
 }
